244599f387d1da1864145050682c2bb333d5399f
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-09 15:27:50 +1100

    Part two complete

--- 2020/day07/main.go
+++ 2020/day07/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -18,6 +19,26 @@ func canHold(s string, set []string) bool {
 	return false
 }
 
+var lines = map[string][]string{}
+
+func getChildCount(bags []string) int {
+
+	count := 1
+	for _, bag := range bags {
+		words := strings.Split(strings.TrimSpace(bag), " ")
+		if len(words) <= 2 {
+			break
+		}
+		newBag := strings.Split(strings.Join(words[1:3], " "), ",")
+		num, _ := strconv.Atoi(words[0])
+		bagName := strings.Join(newBag, " ")
+		if !strings.Contains(bagName, "other bags.") {
+			count += num * getChildCount(lines[bagName])
+		}
+	}
+	return count
+}
+
 func main() {
 	file, err := os.Open("input")
 	if err != nil {
@@ -27,46 +48,17 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	data := map[string][]string{}
+	seen := map[string]struct{}{}
 	for scanner.Scan() {
 		tmp := scanner.Text()
 		// split on space
 		words := strings.Split(tmp, " ")
 		bags := strings.Split(strings.Join(words[4:], " "), ",")
-		data[strings.Join(words[:2], " ")] = bags
+		lines[strings.Join(words[:2], " ")] = bags
 	}
 
-	possibles := map[string]struct{}{}
-	seen := map[string]struct{}{}
-	for k := range data {
-		// does this k have shiny gold bags
-		if canHold("shiny gold", data[k]) {
-			possibles[k] = struct{}{}
-			seen[k] = struct{}{}
-		}
-	}
-
-	fmt.Println(len(possibles))
-	newPossibles := map[string]struct{}{}
-	for {
-		changed := false
-		for k := range possibles {
-			for j := range data {
-				if canHold(k, data[j]) {
-					if _, ok := seen[j]; !ok {
-						changed = true
-						seen[j] = struct{}{}
-					}
-					newPossibles[j] = struct{}{}
-				}
-			}
-		}
-		possibles = newPossibles
-		if !changed {
-			break
-		}
-	}
-	fmt.Println(len(seen))
+	seen["shiny gold"] = struct{}{}
+	fmt.Println(getChildCount(lines["shiny gold"]) - 1)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
